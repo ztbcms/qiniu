77d1c3da2141776638daa000e61f66068609cb77
Jayin <tonjayin@gmail.com>
2023-05-31 15:12:26 +0800

    update

--- controller/Upload.php
+++ controller/Upload.php
@@ -78,25 +78,26 @@ class Upload extends BaseApi
         //存储服务回调的url，具体可以参考：http://developer.qiniu.com/docs/v6/api/reference/security/put-policy.html
         $url = api_url('/qiniu/upload/callback');
         $isQiniuCallback = $auth->verifyCallback($contentType, $authorization, $url, $callbackBody);
-        $upload_file = [
-            'uuid' => generateUniqueId(),
-            'bucket' => $config['bucket'],
-            'key' => $key,
-            'file_name' => $fname,
-            'file_type' => $mimeType,
-            'file_size' => $fsize,
-            'file_url' => $config['domain'] . '/' . $key,
-            'create_time' => time(),
-        ];
         if ($isQiniuCallback) {
+            $upload_file = [
+                'bucket' => $config['bucket'],
+                'key' => $key,
+                'file_name' => $fname,
+                'file_type' => $mimeType,
+                'file_size' => $fsize,
+                'file_url' => $config['upload']['domain'] . '/' . $key,
+                'create_time' => time(),
+            ];
             // 记录上传信息
             $exist = QiniuUploadFileModel::where([
                 'bucket' => $bucket,
                 'key' => $upload_file['key']
             ])->find();
             if ($exist) {
+                $upload_file['uuid'] = $exist['uuid'];
                 return self::makeJsonReturn(true, $upload_file);
             }
+            $upload_file['uuid'] = generateUniqueId();
             $model = new QiniuUploadFileModel();
             $model->data($upload_file)->save();
             return self::makeJsonReturn(true, $upload_file);
