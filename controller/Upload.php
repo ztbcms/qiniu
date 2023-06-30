25be29a528e875cddc943a2e51bf926db12700ef
Jayin <tonjayin@gmail.com>
2023-07-01 01:02:33 +0800

    refactor

--- controller/Upload.php
+++ controller/Upload.php
@@ -13,7 +13,7 @@ use think\Request;
 
 class Upload extends BaseApi
 {
-    protected $skillAuthActions = ['getUploadConfig', 'callback', 'getUploadConfigV2', 'callbackV2'];
+    protected $skillAuthActions = ['getUploadConfig', 'callback', 'getUploadConfigV2'];
 
     /**
      * 上传配置
