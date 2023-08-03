3b789601d9c83d6e25c9a145a91419cec82cca65
Jayin <tonjayin@gmail.com>
2023-08-03 18:06:57 +0800

    Add constant varb

--- model/QiniuUploadFileModel.php
+++ model/QiniuUploadFileModel.php
@@ -10,4 +10,13 @@ use think\Model;
 class QiniuUploadFileModel extends Model
 {
     protected $name = 'qiniu_upload_file';
+
+    /**
+     * 文件状态:正常，启用
+     */
+    const FILE_STATUS_OK = 0;
+    /**
+     * 文件状态:禁用
+     */
+    const FILE_STATUS_DISABLE = 1;
 }
