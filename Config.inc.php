b8e3c19022170fb65a318175face91a925b8c6c6
Jayin <tonjayin@gmail.com>
2024-06-26 23:24:31 +0800

    Bump version🚀

--- Config.inc.php
+++ Config.inc.php
@@ -2,11 +2,11 @@
 
 return [
     //模块名称
-    'modulename'  => '七牛',
+    'modulename'  => '七牛云',
     //图标
     'icon'        => 'https://dn-coding-net-production-pp.qbox.me/e57af720-f26c-4f3b-90b9-88241b680b7b.png',
     //模块简介
-    'introduce'   => '',
+    'introduce'   => '七牛云组件，对接上传，抓取',
     //模块介绍地址
     'address'     => 'http://doc.ztbcms.com/module/',
     //模块作者
@@ -16,7 +16,7 @@ return [
     //作者邮箱
     'authoremail' => 'tonjayin@gmail.com',
     //版本号，请不要带除数字外的其他字符
-    'version'     => '1.0.0',
+    'version'     => '1.0.1',
     //适配最低CMS版本，
     'adaptation'  => '4.0.0',
     //依赖模块
